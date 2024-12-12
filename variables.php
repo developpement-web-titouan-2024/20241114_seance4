b70a7905b5168b585d7e68f1c666528780fafd93
unknown <titouan.nicoullaud@gmail.com>
2024-12-12 16:51:20 +0100

    ajout d'un mot de passe

--- variables.php
+++ variables.php
@@ -5,17 +5,21 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'devine',
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'MiamMiam',
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => 'laCasto28',
     ],
+    
 ];
 
 $recipes = [
